8b5f1a876aca6e39837d906626b7497d13b0b227
frankvanpraag <16331016+frankvanpraag@users.noreply.github.com>
2021-02-05 22:49:21 +1100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def view_card(name, title, company, mobile, email, addr1, locality, state, postc
 	return render_template('card.html', name=name, title=title, company=company, mobile=mobile, email=email, addr1=addr1, locality=locality, state=state, postcode=postcode, country=country, photourl=photourl, officenumber=officenumber, linkedin=linkedin, github=github, facebook=facebook)
 
 @app.route('/card2/name=<string:name>&title=<string:title>&company=<string:company>&mobile=<string:mobile>&email=<string:email>&addr1=<string:addr1>&locality=<string:locality>&state=<string:state>&postcode=<string:postcode>&country=<string:country>&photourl=<string:photourl>&officenumber=<string:officenumber>&linkedin=<string:linkedin>&github=<string:github>&facebook=<string:facebook>')
-def view_card(name, title, company, mobile, email, addr1, locality, state, postcode, country, photourl, officenumber, linkedin, github, facebook):
+def view_card2(name, title, company, mobile, email, addr1, locality, state, postcode, country, photourl, officenumber, linkedin, github, facebook):
 	return render_template('card2.html', name=name, title=title, company=company, mobile=mobile, email=email, addr1=addr1, locality=locality, state=state, postcode=postcode, country=country, photourl=photourl, officenumber=officenumber, linkedin=linkedin, github=github, facebook=facebook)
 
 if __name__ == '__main__':
